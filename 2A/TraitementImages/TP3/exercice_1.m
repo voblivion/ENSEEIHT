a2c7b5f4820f2e91842cdf70782a4d2ad8aceb10
Drouin viallard Victor <vdrouinv@localhost.localdomain>
2016-03-25 15:56:58 +0100

    TIM - TP

---
+++ 2A/TraitementImages/TP3/exercice_1.m
@@ -0,0 +1,42 @@
+clear;
+close all;
+taille_ecran = get(0,'ScreenSize');
+L = taille_ecran(3);
+H = taille_ecran(4);
+
+% Parametres communs a tous les tests :
+reduction = 0.25;			% Facteur de reduction de la resolution
+k_max = 100; 
+epsilon = 0.1;
+chemin = 'Images/';
+image = {'oignon.png','piments.png','pommes.png','chat.png'};
+valeurs_T = [15 20 15 15];		% Parametres a ajuster
+valeurs_h = [20 15 15 15];		% Parametres a ajuster
+position = {[0,0.5*H,0.5*L,0.37*H],[0.5*L,0.5*H,0.5*L,0.37*H],[0,0,0.5*L,0.37*H],[0.5*L,0,0.5*L,0.37*H]};
+
+for i = 1:length(image)
+	nom_fichier = fullfile('Images/', image{i});
+	I = imread(nom_fichier);
+	I = imresize(I,reduction);
+	f = figure('Name',nom_fichier,'Position',position{i});
+	subplot(1,2,1);
+	imagesc(I);
+	axis off;
+	axis equal;
+	T = valeurs_T(i);
+	h = valeurs_h(i);
+	t = title(['$(T,h) = ($' num2str(T) '$,$' num2str(h) ')'],'Interpreter','Latex','FontSize',20); 
+	pos = get(t,'position');
+ 	set(t,'position', pos+[0 2 0]);
+	drawnow;
+
+	[S,k] = meanshift(I,T,h,k_max,epsilon);
+	disp([nom_fichier ' : ' num2str(k) ' iterations']);
+    figure(f)
+	subplot(1,2,2);
+	imagesc(S);
+	axis off;
+	axis equal;
+%	nb_regions = comptage(...);
+%	title([num2str(nb_regions) ' regions detectees'],'Interpreter','Latex','FontSize',20);
+end
---
+++ 2A/TraitementImages/TP3/luv2rgb.m
@@ -0,0 +1,35 @@
+% LUV2RGB conversion LUV vers RGB
+
+function rgb = luv2rgb(luv)
+
+% Conversion en XYZ :
+
+xyz = luv;
+
+Yn = 1;
+Un_prime = 0.19784977571475;
+Vn_prime = 0.46834507665248;
+
+u_prime	= luv(2,:)./(13*luv(1,:))+Un_prime;
+v_prime	= luv(3,:)./(13*luv(1,:))+Vn_prime;
+
+xyz(2,:) = (luv(1,:)<8.0).*(Yn*luv(1,:)/903.3)+(luv(1,:)>=8.0).*(Yn*(((luv(1,:)+16.0)/116.0).^3));
+xyz(1,:) = 9*u_prime.*xyz(2,:)./(4*v_prime);
+xyz(3,:) = (12-3*u_prime-20*v_prime).*xyz(2,:)./(4*v_prime);
+
+% Conversion en RGB :
+
+RGB = [3.2405, -1.5371, -0.4985; ...
+       -0.9693,  1.8760,  0.0416; ...
+       0.0556, -0.2040,  1.0573];
+rgb = RGB*xyz;
+
+% Verifications :
+
+rgb(1,:) = (luv(1,:)>=0.1).*rgb(1,:);
+rgb(2,:) = (luv(1,:)>=0.1).*rgb(2,:);
+rgb(3,:) = (luv(1,:)>=0.1).*rgb(3,:);
+
+rgb(1,:) = (rgb(1,:)>=0 & rgb(1,:)<=1).*rgb(1,:)+(rgb(1,:)>1);
+rgb(2,:) = (rgb(2,:)>=0 & rgb(2,:)<=1).*rgb(2,:)+(rgb(2,:)>1);
+rgb(3,:) = (rgb(3,:)>=0 & rgb(3,:)<=1).*rgb(3,:)+(rgb(3,:)>1);
