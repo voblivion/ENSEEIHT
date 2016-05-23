1c1cb07b46ef2311712f07354108caead4cc2b64
Drouin viallard Victor <vdrouinv@localhost.localdomain>
2016-05-24 00:49:15 +0200

    EDO - TP1 - amélioration de phi
    Peut désormais être appelée pour plusieurs évaluations imultanées.

--- 2A/EquationsDifferentiellesOrdinaires/TP1/phi.m
+++ 2A/EquationsDifferentiellesOrdinaires/TP1/phi.m
@@ -1,8 +1,8 @@
 function [dy] = phi(t, y)
 
 dy = zeros(size(y));
-dy(1) = y(2);
-dy(2) = (1 - y(1)^2) * y(2) - y(1);
+dy(:, 1) = y(:, 2);
+dy(:, 2) = (1 - y(:, 1).^2) .* y(:, 2) - y(:, 1);
 
 end
 
