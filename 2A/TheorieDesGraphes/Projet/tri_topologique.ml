fde446656f71f699885202b0e7b083404d97ac06
Drouin viallard Victor <vdrouinv@localhost.localdomain>
2016-06-02 18:42:28 +0200

    TDG - Projet - tri topologique

---
+++ 2A/TheorieDesGraphes/Projet/tri_topologique.ml
@@ -0,0 +1,45 @@
+module G = Graph.Pack.Digraph;;
+module V = G.V;;
+module M = G.Mark;;
+module S = Set.Make(G.V);;
+module E = G.E;;
+
+let creer_graph_oriente lv lep =
+    let go = G.create () in begin
+        List.iter (fun t -> G.add_vertex go (V.create t)) lv;
+        List.iter (fun (v1, v2, p12) ->
+            G.add_edge_e go (E.create (G.find_vertex go v1) p12 (G.find_vertex go v2))) lep;
+        go;
+    end
+;;
+
+let print_vertex fmt v = Format.fprintf fmt "%i" (V.label v);;
+
+(* sans_dependance
+ * Retourne la liste des noeuds d'un graphe orienté qui n'ont pas de prédécesseurs.
+ *
+ * IN : g le graphe dans lequel retrouver les noeuds
+ * OUT : l la liste des noeuds sans prédécesserus du graphe
+ *)
+let sans_dependance g = G.fold_vertex (fun v l ->
+    if G.in_degree g v = 0 then v::l else l) g []
+;;
+
+(* a_pred_numerotes
+ * Détermine si le noeud donné a tous ses prédécesseurs dans la liste donnée.
+ *
+ * IN : g le graphe, z la liste, v le noeud
+ * OUT : true ssi tous les prédécesseurs de v dans g sont dans z
+ *)
+let a_pred_numerotes g z v = List.for_all (fun v ->
+    List.mem v z) (G.pred g v)
+;;
+
+let tri_topologique g =
+    let y = sans_dependance g in
+    let rec aux z = function
+        | [] -> z
+        | v::y -> aux (v::z) (G.fold_succ (fun w y ->
+            if (a_pred_numerotes g (v::z) w) then (w::y) else y) g v y)
+    in List.rev (aux [] y)
+;;
